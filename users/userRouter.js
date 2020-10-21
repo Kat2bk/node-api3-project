97067affe2d9faa2b01413fb63cd766d4aca1c1a
unknown <Katherine2bk@gmail.com>
2020-10-21 15:40:11 -0700

    finished three more endpoints for userRouter... router.post for user, router.post for user posts, router.get to get list of users, router.get for specific user, router.get for specific user posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -17,20 +17,55 @@ router.post('/', validateUser, (req, res) => {
   })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const postInfo = {
+    text: req.body.text,
+    user_id: req.params.id
+  }
+
+posts.insert(postInfo)
+  .then(post => {
+    res.status(201).json(post)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
 router.get('/', (req, res) => {
   // do your magic!
+  users.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
+  const {id} = req.params;
   // do your magic!
+  users.getById(id)
+  .then(user => {
+    res.status(200).json(user)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const {id} = req.params;
+  users.getUserPosts(id)
+  .then(posts => {
+    res.status(200).json(posts)
+  })
+  .catch(error => {
+    next(error)
+  })
 });
 
 router.delete('/:id', (req, res) => {
@@ -72,6 +107,17 @@ function validateUser(req, res, next) {
 
 function validatePost(req, res, next) {
   // do your magic!
+  if (req.body && req.body.text) {
+    next()
+  } else if (req.body && !req.body.text) {
+    res.status(400).json({
+      message: "Missing required field text"
+    })
+  } else {
+    res.status(400).json({
+      message: "Missing post data"
+    })
+  }
 }
 
 module.exports = router;
